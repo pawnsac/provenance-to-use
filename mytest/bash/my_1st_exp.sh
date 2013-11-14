cb7b75baadba6322de6ceb1539fcafd7405fd712
Quan Pham <quanpt@gmail.com>
2013-11-14 04:54:47 +0000

    use simple multi repo test
    git-svn-id: file:///home/quanpt/svnrep/cde@60 d5ca3f82-63d5-4c06-abdd-b62558c2a7bc

--- mytest/bash/my_1st_exp.sh
+++ mytest/bash/my_1st_exp.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-echo Exp1 Result:
-echo "1st exp" > data.1exp.txt
-cat data.1exp.txt
-cp data.1exp.txt out1.txt
-date >> out1.txt
-sleep 1
-echo "done 1st exp"
-cat out1.txt
-pwd
-echo "======="
-
+echo Exp1
+python process.py hello.txt out.from.exp1.txt
+md5sum out.from.exp1.txt
+echo Exp1.done
--- mytest/bash/my_2nd_exp.sh
+++ mytest/bash/my_2nd_exp.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
+echo Exp2
 ./s02.rerun_1st_exp.sh
-echo "Exp2 Result: "
-#cp cde-package/cde-root/home/quanpt/assi/cde/mytest/bash/data.1exp.txt ./
-echo "this is 2nd experiment data" > data.2exp.txt
-cp data.2exp.txt temp.txt
-cat temp.txt > out.txt
-echo "===" >> out.txt
-cat out.txt
-ls *.txt
-pwd
+cp cde-package/cde-root/home/quanpt/Tools/cde/mytest/bash/out.from.exp1.txt ./in.for.exp2.txt
+md5sum in.for.exp2.txt
+echo Exp2.done
