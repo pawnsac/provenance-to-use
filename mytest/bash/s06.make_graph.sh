8a8b85211aef423bfce89548af4c73b17dd4c301
Quan Pham <quanpt@gmail.com>
2014-01-14 05:46:30 +0000

    add db2prov
    git-svn-id: file:///home/quanpt/svnrep/cde@83 d5ca3f82-63d5-4c06-abdd-b62558c2a7bc

--- mytest/bash/s06.make_graph.sh
+++ mytest/bash/s06.make_graph.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 cd cde-package
+#PROV2DOT=../../../scripts/prov2dot.py
+PROV2DOT=../../../scripts/db2dot.py
 if [ ! -d gv ]
 then
-  ../../../scripts/prov2dot.py -f provenance.cde-root-Y6*.1.log --withgraph
+  $PROV2DOT -f provenance.cde-root-Y6*.1.log --withgraph
   cat provenance.cde-root-YP*.1.log  provenance.cde-root-Y6*.2.log > prov.log
-  ../../../scripts/prov2dot.py -f prov.log -d gv2
-  ../../../scripts/prov2dot.py -f provenance.cde-root-Y6*.2.log -d gv3 --withgraph
+#  $PROV2DOT -f prov.log -d gv2
+  $PROV2DOT -f provenance.cde-root-Y6*.2.log -d gv3 --withgraph
 fi
 #../../../scripts/graphcmp.py gv/main.graph gv3/main.graph
-dot -Tpng gv/main.gv -o exp1.png
-dot -Tpng gv2/main.gv -o exp2.png
-mv exp*.png ~/html/
+#dot -Tpng gv/main.gv -o exp1.png
+#dot -Tpng gv2/main.gv -o exp2.png
+#mv exp*.png ~/html/
