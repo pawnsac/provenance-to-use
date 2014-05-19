892b4434e0f7db6f4598e5a0e45eab8b7e99cd56
Quan Pham <quanpt@gmail.com>
2014-05-19 14:41:10 +0000

    Use THIS VERSION of mytest/bash to demo IPAW14
    git-svn-id: file:///home/quanpt/svnrep/cde@160 d5ca3f82-63d5-4c06-abdd-b62558c2a7bc

--- mytest/bash/s06.make_graph.sh
+++ mytest/bash/s06.make_graph.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 cd cde-package
-#PROV2DOT=../../../scripts/prov2dot.py
-PROV2DOT=../../../scripts/db2dot.py
+PROV2DOT=../../../scripts/prov2dot.py
+#PROV2DOT=../../../scripts/db2dot.py
 if [ ! -d gv ]
 then
   $PROV2DOT -f provenance.cde-root-Y6*.1.log --withgraph
   cat provenance.cde-root-YP*.1.log  provenance.cde-root-Y6*.2.log > prov.log
-#  $PROV2DOT -f prov.log -d gv2
+  $PROV2DOT -f prov.log -d gv2
   $PROV2DOT -f provenance.cde-root-Y6*.2.log -d gv3 --withgraph
 fi
 #../../../scripts/graphcmp.py gv/main.graph gv3/main.graph
-#dot -Tpng gv/main.gv -o exp1.png
-#dot -Tpng gv2/main.gv -o exp2.png
-#mv exp*.png ~/html/
+dot -Tpng gv/main.gv -o exp1.png
+dot -Tpng gv2/main.gv -o exp2.png
+mv exp*.png ~/html/
