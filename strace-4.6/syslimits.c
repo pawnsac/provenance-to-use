26d7795af24098be9a9d9b6cb1bc41184abc45c5
digimokan <digimokan@123mail.org>
2017-07-21 14:45:06 -0500

    Link syslimits mod to project, fix max_open_files
    - In provenance-to-use/strace-4.6/
        * $ aclocal
        * $ automake --add-missing
        * add syslimits.h and syslimits.c to Makefile.am
    - Fix some errors, names in max_open_files function.

--- strace-4.6/syslimits.c
+++ strace-4.6/syslimits.c
@@ -22,27 +22,27 @@ ref:      Advanced Programming In The UNIX Environment, 3rd Ed, Section 2.6
 
 // if OPEN_MAX is defined, we're done.  else obtain the max programatically.
 #ifdef OPEN_MAX
-static long open_max = OPEN_MAX;
+static long max_open = OPEN_MAX;
 #else
-static long open_max = 0;
+static long max_open = 0;
 #endif
 
 // if unable to get the max by any means, use this number, which is *probably*
 // greater than the undiscoverable actual max on the system (according to APUE)
-static const long open_max_fallback = 256;
+static const long max_open_fallback = 256;
 
 long max_open_files (void) {
 
   // OPEN_MAX not defined: obtain it once, programatically.
-  if (open_max == 0) {
+  if (max_open == 0) {
 
     // reset errno
     errno = 0;
 
-    if ((open_max = sysconf(_SC_OPEN_MAX)) < 0) {
+    if ((max_open = sysconf(_SC_OPEN_MAX)) < 0) {
       // sysconf did not set errno value: limit is indeterminate
-      if (errno = 0) {
-        open_max = open_max_fallback;
+      if (errno == 0) {
+        max_open = max_open_fallback;
       // sysconf set an errno value: problem executing sysconf
       } else {
         perror("sysconf call in max_open_files()");
@@ -51,5 +51,6 @@ long max_open_files (void) {
 
   }
 
+  return max_open;
 }
 
